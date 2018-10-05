3ffbac8b38d4465082c42cc43d7e83741a066e11
Mat <mat.frayne@gmail.com>
2018-10-05 23:15:35 +0930

    .

--- setup.py
+++ setup.py
@@ -1,20 +1,27 @@
-import setuptools
+from setuptools import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
 
-setuptools.setup(
-    name="PyDebug",
-    version="0.0.1",
-    author="Mat-Frayne",
-    description="I'm Learning :)",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/Mat-Frayne/PythonDebugger",
-    packages=setuptools.find_packages(),
-    classifiers=[
+requirements = []
+with open('requirements.txt') as f:
+  requirements = f.read().splitlines()
+
+readme = ''
+with open('README.md') as f:
+    readme = f.read()
+
+setup(name='PyDebug',
+      author='Mat Frayne',
+      url='https://github.com/Mat-Frayne/PythonDebugger',
+      version="0.0.1",
+      packages=['PyDebug'],
+      license='MIT',
+      description='Im Learning',
+      long_description=readme,
+      include_package_data=True,
+      install_requires=requirements,
+      classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ],
+    ]
 )
